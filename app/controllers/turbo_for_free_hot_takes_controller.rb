abfa3719e331047c34430d6e9872ac67e1c64e11
Stephen Best <bestie@gmail.com>
2021-06-03 13:09:06 +0200

    Turbo hot takes for free

---
+++ app/controllers/turbo_for_free_hot_takes_controller.rb
@@ -0,0 +1,24 @@
+class TurboForFreeHotTakesController < ApplicationController
+  # GET /hot_takes
+  def index
+    @hot_takes = HotTake.all
+    @hot_take = HotTake.new
+  end
+
+  # POST /hot_takes
+  def create
+    @hot_take = HotTake.new(hot_take_params)
+
+    if @hot_take.save
+      redirect_to turbo_for_free_hot_takes_path
+    else
+      @hot_takes = HotTake.all
+      render :index
+    end
+  end
+
+  private
+    def hot_take_params
+      params.require(:hot_take).permit(:handle, :content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/", to: "hot_takes#index"
 
   resources :hot_takes, only: [:index, :create]
+  resources :turbo_for_free_hot_takes, only: [:index, :create]
 end
